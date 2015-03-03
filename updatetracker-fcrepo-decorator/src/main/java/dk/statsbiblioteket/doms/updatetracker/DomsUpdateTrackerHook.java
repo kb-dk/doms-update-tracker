45eecdfcad20d6fea28592515bf8964a92b0065a
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2015-03-03 11:10:27 +0100

    Review followup

--- updatetracker-fcrepo-decorator/src/main/java/dk/statsbiblioteket/doms/updatetracker/DomsUpdateTrackerHook.java
+++ updatetracker-fcrepo-decorator/src/main/java/dk/statsbiblioteket/doms/updatetracker/DomsUpdateTrackerHook.java
@@ -1,6 +1,5 @@
 package dk.statsbiblioteket.doms.updatetracker;
 
-import dk.statsbiblioteket.doms.updatetracker.Database.MethodName;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.fcrepo.server.Context;
@@ -13,15 +12,19 @@ import org.fcrepo.server.proxy.ModuleConfiguredInvocationHandler;
 import org.fcrepo.server.storage.ConnectionPool;
 import org.fcrepo.server.storage.ConnectionPoolManager;
 
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Date;
 
-import static dk.statsbiblioteket.doms.updatetracker.Database.MethodName.CREATE;
-import static dk.statsbiblioteket.doms.updatetracker.Database.MethodName.DELETE;
-import static dk.statsbiblioteket.doms.updatetracker.Database.MethodName.RELATIONS;
-import static dk.statsbiblioteket.doms.updatetracker.Database.MethodName.UPDATE;
-
+/**
+ * The update tracker fedora hook. This hook stores information about all changing methods in a database, for later
+ * replay in the update tracker.
+ * The databse is chosen via the fedora ConnectionPoolManager. Set the variable updateTrackerPoolName alongside the
+ * decorator to specify which pool should be used.
+ * Only changing operations are hooked, but all operations are logged on info level, as an extra precausion.
+ */
 public class DomsUpdateTrackerHook extends AbstractInvocationHandler implements ModuleConfiguredInvocationHandler {
 
     /** Logger for this class. */
@@ -31,18 +34,14 @@ public class DomsUpdateTrackerHook extends AbstractInvocationHandler implements 
 
     @Override
     public void init(Server server) throws InitializationException {
-        ConnectionPoolManager cpm = (ConnectionPoolManager) server.getModule("org.fcrepo.server.storage" +
-                                                                             ".ConnectionPoolManager");
+        ConnectionPoolManager cpm = (ConnectionPoolManager) server.getModule("org.fcrepo.server.storage.ConnectionPoolManager");
         if (cpm == null) {
-            throw new InitializationException("ConnectionPoolManager module was required, but apparently has " +
-                                                    "not been loaded.");
+            throw new InitializationException("ConnectionPoolManager module was required, but apparently has not been loaded.");
         }
 
-        ManagementModule managementModule = (ManagementModule) server.getModule("org.fcrepo.server.management" +
-                                                                                ".Management");
+        ManagementModule managementModule = (ManagementModule) server.getModule("org.fcrepo.server.management.Management");
         if (managementModule == null) {
-            throw new InitializationException("ManagementModule module was required, but apparently has " +
-                                              "not been loaded.");
+            throw new InitializationException("ManagementModule module was required, but apparently has not been loaded.");
         }
 
 
@@ -50,7 +49,7 @@ public class DomsUpdateTrackerHook extends AbstractInvocationHandler implements 
         ConnectionPool cPool = null;
         try {
             if (cPoolName == null){
-                logger.debug("connectionPool unspecified; using default from " + "ConnectionPoolManager.");
+                logger.debug("connectionPool unspecified; using default from ConnectionPoolManager.");
                 cPool = cpm.getPool();
             } else {
                 logger.debug("connectionPool specified: " + cPoolName);
@@ -67,49 +66,96 @@ public class DomsUpdateTrackerHook extends AbstractInvocationHandler implements 
         }
     }
 
+
     @Override
-    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+    public Object invoke(Object proxy, Method method, Object[] args) throws
+                                                                     InvocationTargetException,
+                                                                     IllegalAccessException {
 
-        logger.debug("Entering method invoke in FedoraModifyObjectHook with arguments: method='" + method.getName() +
-                     "' and arguments: " + Arrays.toString(args));
-        //Invoke, then hook
-        Object result = method.invoke(target, args);
 
-        Context context = (Context) args[0];
-        Date now = Server.getCurrentDate(context);
-        String pid = args[1].toString();
-        logger.info("The method was called with the pid " + pid);
+        final String methodName = method.getName();
 
-        String s = method.getName();
-        if (s.equals("ingest")) {
-            database.addLogEntry(pid, now, CREATE, null);
-        } else if (s.equals("modifyObject")) {
-            String state = (String) args[2];
-            if (state == null) {
-                database.addLogEntry(pid, now, UPDATE, null);
-            } else {
-                if (state.equals("D")) {
-                    database.addLogEntry(pid, now, DELETE, null);
-                } else {
-                    database.addLogEntry(pid, now, MethodName.STATE, state);
-                }
+        String pid;
+        Date now;
+        String param;
+        try {
+            Context context = (Context) args[0];
+            now = Server.getCurrentDate(context);
+            pid = args[1].toString();
+            param = null;
+            if (args.length > 2) {
+                param = args[2].toString();
             }
-        } else if (s.equals("purgeObject")) {
-            database.addLogEntry(pid, now, DELETE, null);
-        } else if (s.equals("addDatastream") || s.equals("modifyDatastreamByReference") ||
-                   s.equals("modifyDatastreamByValue") || s.equals("purgeDatastream") ||
-                   s.equals("setDatastreamState") || s.equals("setDatastreamVersionable")) {
-            String dsid = (String) args[2];
-            if (dsid.equals("RELS-EXT")) {
-                database.addLogEntry(pid, now, RELATIONS, null);
-            } else {
-                database.addLogEntry(pid, now, UPDATE, dsid);
-            }
-        } else if (s.equals("addRelationship") || s.equals("purgeRelationship")) {
-            database.addLogEntry(pid, now, RELATIONS, null);
-        } else {
-            //Unimportant operation
+        } catch (Exception e) {
+            logger.error("Failed to parse params '" + Arrays.toString(args) + "'", e);
+            return method.invoke(proxy, args);
         }
-        return result;
+
+        logger.info("Method: " + methodName + "(" + pid + ", " + now.getTime() + ", " + param + ")");
+
+        switch (methodName) {
+            case "ingest":
+            case "modifyObject":
+            case "purgeObject":
+            case "addDatastream":
+            case "modifyDatastreamByReference":
+            case "modifyDatastreamByValue":
+            case "purgeDatastream":
+            case "setDatastreamState":
+            case "setDatastreamVersionable":
+            case "addRelationship":
+            case "purgeRelationship":
+                return invokeHook(method, args, methodName, pid, now, param);
+
+            case "getObjectXML":
+            case "export":
+            case "getDatastream":
+            case "getDatastreams":
+            case "getDatastreamHistory":
+            case "putTempStream":
+            case "getTempStream":
+            case "compareDatastreamChecksum":
+            case "getNextPID":
+            case "getRelationships":
+            case "validate":
+                return method.invoke(target, args);
+
+            default:
+                logger.warn("Unknown method invoked  '" + methodName + "'");
+                return method.invoke(target, args);
+        }
+    }
+
+    /**
+     * First, add the log entry. Then invoke the method. Should the method fail, remove the log entry
+     * @param method the method
+     * @param args args for the method
+     * @param methodName the name of the method
+     * @param pid the pid of the object
+     * @param now timestamp of the invocation
+     * @param param the first param, can be null
+     * @return the result of the method invocation
+     * @throws IllegalAccessException
+     * @throws InvocationTargetException
+     */
+    private Object invokeHook(Method method, Object[] args, String methodName, String pid, Date now,
+                              String param) throws IllegalAccessException, InvocationTargetException {
+        Long logkey = -1L;
+        try {
+            logkey = database.addLogEntry(pid, now, methodName, param);
+        } catch (IOException e) {
+            logger.error("Failed to add log to database",e);
+        }
+
+        try {
+            return method.invoke(target, args);
+        } catch (Throwable tr) {
+            try {
+                database.removeLogEntry(logkey);
+            } catch (IOException e) {
+                logger.error("Failed to remove log key '"+logkey+"' from database",e);
+            }
+            throw tr;
+        }
     }
 }
