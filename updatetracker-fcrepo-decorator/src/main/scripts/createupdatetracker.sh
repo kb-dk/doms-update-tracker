5681d155bbae9b61c48ee96cdfa8fe94022f456a
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2015-03-16 14:26:41 +0100

    Review followup

--- updatetracker-fcrepo-decorator/src/main/scripts/createupdatetracker.sh
+++ updatetracker-fcrepo-decorator/src/main/scripts/createupdatetracker.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
-# Dump database table with timestamps to a file
-psql domsTripleStore -c "\copy (SELECT * FROM t1 ORDER BY o) TO STDOUT" > alltimestamps
-# Remove old input file if any
-rm -f updatetrackerdata
-# Read each timestamp
-i = 0
-cat alltimestamps | while read object date; do
+# Dump database table with timestamps to a file. Note: It should be checked that t1 is the correct table beforehand.
+echo "Dumping all lastModified triples from database"
+lastmodifiedtable=$(psql domsTripleStore -c "\copy (SELECT pkey FROM tmap WHERE p='<info:fedora/fedora-system:def/view#lastModifiedDate>') TO STDOUT")
+psql domsTripleStore -c "\copy (SELECT * FROM t$lastmodifiedtable ORDER BY o) TO STDOUT" > alltimestamps
+echo "Done dumping. Now parsing triples."
+# Rename old input file if any
+if [[ -e updatetrackerdata ]]; then mv updatetrackerdata updatetrackerdata.$(date -Iseconds)
+
+# Read each timestamp. Format looks like this:
+#   <info:fedora/uuid:b994d7bf-7911-4413-8475-bb8dc3f7b804>	"2014-10-21T04:26:13.987Z"^^<http://www.w3.org/2001/XMLSchema#dateTime>
+#   <info:fedora/uuid:ad4796af-4b75-4691-a784-ffe8e2833936/DC>	"2014-12-19T17:42:22.25Z"^^<http://www.w3.org/2001/XMLSchema#dateTime>
+#   <info:fedora/uuid:bec3f8e5-b632-449f-b4dc-8a307ded2bd2/RELS-EXT>	"2014-12-26T16:21:47.837Z"^^<http://www.w3.org/2001/XMLSchema#dateTime>
+#   <info:fedora/uuid:b07d26be-022f-480a-aff4-480f32df401a>	"2015-03-02T00:28:57.77Z"^^<http://www.w3.org/2001/XMLSchema#dateTime>
+i=0
+cat alltimestamps | while read object timestamp; do
     # Extract object name
     object=${object##<info:fedora/}
     object=${object%%>}
     # Skip if this is a datastream, not an object
     if [[ "$object" == */* ]]; then continue; fi
-    # Extract date
-    date=${date##\"}
-    date=${date%%\"^^<http://www.w3.org/2001/XMLSchema#dateTime>}
-    #date=$(date -d "${date/T/ }" +%s%3N)
+    # Extract timestamp
+    timestamp=${timestamp##\"}
+    timestamp=${timestamp%%\"^^<http://www.w3.org/2001/XMLSchema#dateTime>}
+    #timestamp=$(date -d "${timestamp/T/ }" +%s%3N)
     # Add to update tracker log
-    echo -e $object\\t$date\\tingest >> updatetrackerdata
+    echo -e ${object}\\t${timestamp}\\t"ingest" >> updatetrackerdata
     # Report progress
     i=$((i+1))
     if [[ $((i%100000)) == 0 ]]; then date; echo "Handled $i timestamps"; fi
 done
-# Ingest into updatetracker log database table
+# Ingest into updatetracker log database table. Assumes table already exists.
+echo "Ingesting all triples into database"
 cat updatetrackerdata | psql avisdoms -c "\copy \"updateTrackerLogs\" (pid, happened, method) from stdin"
+echo "All done."
