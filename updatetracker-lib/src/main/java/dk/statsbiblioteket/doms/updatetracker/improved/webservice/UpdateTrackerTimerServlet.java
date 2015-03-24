53d7dcd0f46ca987acf4813b39a721d3e04eef54
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2015-03-24 11:38:44 +0100

    Doc of update tracker timer servlet

--- updatetracker-lib/src/main/java/dk/statsbiblioteket/doms/updatetracker/improved/webservice/UpdateTrackerTimerServlet.java
+++ updatetracker-lib/src/main/java/dk/statsbiblioteket/doms/updatetracker/improved/webservice/UpdateTrackerTimerServlet.java
@@ -7,6 +7,11 @@ import dk.statsbiblioteket.sbutil.webservices.configuration.ConfigCollection;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
+/**
+ * This is a loadOnStartup Service. It served nothing, and should probably be a Listener
+ * It starts the update tracker system up, when the servlet is loaded. It reads the config from the servlet configuraiton
+ * ie. context params. It closes the update tracker when the system is closed
+ */
 public class UpdateTrackerTimerServlet extends HttpServlet {
 
     public static UpdateTrackingSystem updateTracker;
