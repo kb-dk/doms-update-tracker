cc2c5ad6e4b6d68367b25e2177ddb0edcf63d00d
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2015-03-25 15:03:02 +0100

    Config based on documented constants

--- updatetracker-lib/src/main/java/dk/statsbiblioteket/doms/updatetracker/improved/UpdateTrackingConfig.java
+++ updatetracker-lib/src/main/java/dk/statsbiblioteket/doms/updatetracker/improved/UpdateTrackingConfig.java
@@ -4,6 +4,61 @@ import java.io.File;
 import java.util.Properties;
 
 public class UpdateTrackingConfig {
+    /**
+     * URL to fedora web frontend, ala http://localhost:7880/fedora/objects
+     */
+    protected static final String FEDORA_UPDATETRACKER_WEB_URL = "fedora.updatetracker.web.URL";
+    /**
+     * Username to the fedora web frontend. Do not need to change stuff, but must be able to read all
+     * @see #FEDORA_UPDATETRACKER_WEB_PASSWORD
+     */
+    protected static final String FEDORA_UPDATETRACKER_WEB_USERNAME = "fedora.updatetracker.web.username";
+    /**
+     * Password to the fedora web frontend. Do not need to change stuff, but must be able to read all
+     * * @see #FEDORA_UPDATETRACKER_WEB_USERNAME
+     */
+    protected static final String FEDORA_UPDATETRACKER_WEB_PASSWORD = "fedora.updatetracker.web.password";
+    /**
+     * The database driver to use when talking the worklog database
+     * @see #FEDORA_WORKLOG_DATABASE_URL
+     */
+    protected static final String FEDORA_WORKLOG_DATABASE_DRIVER = "fedora.worklog.database.driver";
+    /**
+     * The jdbc url to fedoras fieldSearchDB, where the worklog is stored per default.
+     */
+    protected static final String FEDORA_WORKLOG_DATABASE_URL = "fedora.worklog.database.URL";
+    /**
+     * Readonly account to the fedora worklog database
+     * @see #FEDORA_WORKLOG_DATABASE_URL
+     */
+    protected static final String FEDORA_WORKLOG_DATABASE_USERNAME = "fedora.worklog.database.username";
+    /**
+     * Readonly account to the fedora worklog database
+     * @see #FEDORA_WORKLOG_DATABASE_URL
+     */
+    protected static final String FEDORA_WORKLOG_DATABASE_PASSWORD = "fedora.worklog.database.password";
+    /**
+     * The delay in starting polling the worklog when starting this service, in
+     * milliseconds. Default 1000ms
+     */
+    protected static final String FEDORA_UPDATETRACKER_DELAY = "fedora.updatetracker.delay";
+    /**
+     * The frequency in milliseconds, which the worklog should be polled. Default 1000ms
+     */
+    protected static final String FEDORA_UPDATETRACKER_PERIOD = "fedora.updatetracker.period";
+    /**
+     * The pagesize to get from the worklog. Default 1000
+     */
+    protected static final String FEDORA_UPDATETRACKER_LIMIT = "fedora.updatetracker.limit";
+    /**
+     * Address of the file to store progress information, i.e. the worklog latest key
+     */
+    protected static final String FEDORA_UPDATETRACKER_PROGRESS_FILE = "fedora.updatetracker.progressFile";
+    /**
+     * The hibernate config file for the update tracker database
+     */
+    protected static final String FEDORA_UPDATETRACKER_HIBERNATE_CONFIG_FILE
+            = "fedora.updatetracker.hibernateConfigFile";
     private final String fedoraWebUrl;
     private final String fedoraWebUsername;
     private final String fedoraWebPassword;
@@ -21,41 +76,37 @@ public class UpdateTrackingConfig {
 
 
     /**
-     * Create a Config object from a java properties. Will look for these keys which are required
-     * fedora.updatetracker.web.URL
-     * fedora.updatetracker.web.username
-     * fedora.updatetracker.web.password
-     * fedora.worklog.database.driver
-     * fedora.worklog.database.URL
-     * fedora.worklog.database.username
-     * fedora.worklog.database.password
-     * fedora.updatetracker.progressFile = /tmp/updateTracker.progress
-     * fedora.updatetracker.hibernateConfigFile
+     * Create a Config object from a java properties.
      *
-     * These properties are optional, and the default value is mentioned
-     * fedora.updatetracker.delay The delay in starting polling the worklog when starting this service, in milliseconds. Default 1000ms
-     * fedora.updatetracker.period The frequency in milliseconds, which the worklog should be polled. Default 1000ms
-     * fedora.updatetracker.limit The pagesize to get from the worklog. Default 1000
-     *
-     * As it stands, the listener will take at most limit workunits from the worklog, and play these against the update
-     * tracker for each period. That means that if doms, on average, do more than limit / period (operations / second), the
-     * update tracker will never catch up.
+     * As it stands, the listener will take at most "limit" workunits from the worklog, and play these against the update tracker for each "period". That means that if doms, on average, do more than limit / period (operations / second), the update tracker will never catch up.
      * @param properties
+     * @see #FEDORA_UPDATETRACKER_WEB_URL
+     * @see #FEDORA_UPDATETRACKER_WEB_USERNAME
+     * @see #FEDORA_WORKLOG_DATABASE_PASSWORD
+     * @see #FEDORA_WORKLOG_DATABASE_DRIVER
+     * @see #FEDORA_WORKLOG_DATABASE_URL
+     * @see #FEDORA_WORKLOG_DATABASE_USERNAME
+     * @see #FEDORA_WORKLOG_DATABASE_PASSWORD
+     * @see #FEDORA_UPDATETRACKER_HIBERNATE_CONFIG_FILE
+     * @see #FEDORA_UPDATETRACKER_PROGRESS_FILE
+     * @see #FEDORA_UPDATETRACKER_DELAY
+     * @see #FEDORA_UPDATETRACKER_PERIOD
+     * @see #FEDORA_UPDATETRACKER_LIMIT
      */
     public UpdateTrackingConfig(Properties properties) {
 
-        this.fedoraWebUrl = properties.getProperty("fedora.updatetracker.web.URL");
-        this.fedoraWebUsername = properties.getProperty("fedora.updatetracker.web.username");
-        this.fedoraWebPassword = properties.getProperty("fedora.updatetracker.web.password");
-        this.fedoraDatabaseDriver = properties.getProperty("fedora.worklog.database.driver");
-        this.fedoraDatabaseURL = properties.getProperty("fedora.worklog.database.URL");
-        this.fedoraDatabaseUsername = properties.getProperty("fedora.worklog.database.username");
-        this.fedoraDatabasePassword = properties.getProperty("fedora.worklog.database.password");
-        this.fedoraUpdatetrackerDelay = Integer.parseInt(properties.getProperty("fedora.updatetracker.delay","1000"));
-        this.fedoraUpdatetrackerPeriod = Integer.parseInt(properties.getProperty("fedora.updatetracker.period","1000"));
-        this.fedoraUpdatetrackerLimit = Integer.parseInt(properties.getProperty("fedora.updatetracker.limit","1000"));
-        this.fedoraUpdatetrackerProgressFile = properties.getProperty("fedora.updatetracker.progressFile","/tmp/updateTracker.progress");
-        this.updatetrackerHibernateConfig = new File(properties.getProperty("fedora.updatetracker.hibernateConfigFile"));
+        this.fedoraWebUrl = properties.getProperty(FEDORA_UPDATETRACKER_WEB_URL);
+        this.fedoraWebUsername = properties.getProperty(FEDORA_UPDATETRACKER_WEB_USERNAME);
+        this.fedoraWebPassword = properties.getProperty(FEDORA_UPDATETRACKER_WEB_PASSWORD);
+        this.fedoraDatabaseDriver = properties.getProperty(FEDORA_WORKLOG_DATABASE_DRIVER);
+        this.fedoraDatabaseURL = properties.getProperty(FEDORA_WORKLOG_DATABASE_URL);
+        this.fedoraDatabaseUsername = properties.getProperty(FEDORA_WORKLOG_DATABASE_USERNAME);
+        this.fedoraDatabasePassword = properties.getProperty(FEDORA_WORKLOG_DATABASE_PASSWORD);
+        this.fedoraUpdatetrackerDelay = Integer.parseInt(properties.getProperty(FEDORA_UPDATETRACKER_DELAY,"1000"));
+        this.fedoraUpdatetrackerPeriod = Integer.parseInt(properties.getProperty(FEDORA_UPDATETRACKER_PERIOD,"1000"));
+        this.fedoraUpdatetrackerLimit = Integer.parseInt(properties.getProperty(FEDORA_UPDATETRACKER_LIMIT,"1000"));
+        this.fedoraUpdatetrackerProgressFile = properties.getProperty(FEDORA_UPDATETRACKER_PROGRESS_FILE,"/tmp/updateTracker.progress");
+        this.updatetrackerHibernateConfig = new File(properties.getProperty(FEDORA_UPDATETRACKER_HIBERNATE_CONFIG_FILE));
     }
 
 
