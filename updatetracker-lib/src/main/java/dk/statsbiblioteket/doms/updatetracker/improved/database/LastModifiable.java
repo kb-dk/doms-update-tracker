72902980f9e20dc76b0e3727e550156994e65331
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2015-04-20 17:17:00 +0200

    LastModifiable done right

--- updatetracker-lib/src/main/java/dk/statsbiblioteket/doms/updatetracker/improved/database/LastModifiable.java
+++ updatetracker-lib/src/main/java/dk/statsbiblioteket/doms/updatetracker/improved/database/LastModifiable.java
@@ -5,4 +5,6 @@ import java.util.Date;
 public interface LastModifiable {
 
     public void setLastModified(Date date);
+
+    public Date getLastModified();
 }
--- updatetracker-lib/src/main/java/dk/statsbiblioteket/doms/updatetracker/improved/database/LastModifiableInterceptor.java
+++ updatetracker-lib/src/main/java/dk/statsbiblioteket/doms/updatetracker/improved/database/LastModifiableInterceptor.java
@@ -12,6 +12,16 @@ public class LastModifiableInterceptor extends EmptyInterceptor {
     @Override
     public boolean onFlushDirty(Object entity, Serializable id, Object[] currentState, Object[] previousState,
                                 String[] propertyNames, Type[] types) {
+        return updateLastModified(entity, currentState, propertyNames);
+    }
+
+    @Override
+    public boolean onSave(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
+        return updateLastModified(entity, state, propertyNames);
+    }
+
+
+    private boolean updateLastModified(Object entity, Object[] currentState, String[] propertyNames) {
         if (entity instanceof LastModifiable) {
             for (int i = 0; i < propertyNames.length; i++) {
                 if ("LASTMODIFIED".equalsIgnoreCase(propertyNames[i])) {
