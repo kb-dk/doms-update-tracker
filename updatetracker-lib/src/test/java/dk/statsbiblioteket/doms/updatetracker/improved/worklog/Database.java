4ecb2a6d367f42aa22de6f6a4718b2b1562f68fa
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2015-06-26 13:07:27 +0200

    Review followup

--- updatetracker-lib/src/test/java/dk/statsbiblioteket/doms/updatetracker/improved/worklog/Database.java
+++ updatetracker-lib/src/test/java/dk/statsbiblioteket/doms/updatetracker/improved/worklog/Database.java
@@ -3,13 +3,19 @@ package dk.statsbiblioteket.doms.updatetracker.improved.worklog;
 import java.io.BufferedReader;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.Statement;
 
+/**
+ * Simple class found at http://www.coderanch.com/t/306966/JDBC/databases/Execute-sql-file-java
+ * Used to create the update tracker logs table and content in the integration tests
+ */
 public class Database {
 
     private static final String DRIVER_NAME = "org.postgresql.Driver";
@@ -42,44 +48,33 @@ public class Database {
         return DriverManager.getConnection(url, user, password);
     }
 
-    public static void executeSQL(File sqlFile) throws SQLException {
+    public static void executeSQL(File sqlFile) throws SQLException, IOException {
         StringBuilder sb = new StringBuilder();
 
-        try {
-            FileReader fr = new FileReader(sqlFile);
-            // be sure to not have line starting with "--" or "/*" or any other non aplhabetical character
+        FileReader fr = new FileReader(sqlFile);
+        // be sure to not have line starting with "--" or "/*" or any other non aplhabetical character
 
-            BufferedReader br = new BufferedReader(fr);
-            String line;
-            while ((line = br.readLine()) != null) {
-                sb.append(line);
-            }
-            br.close();
-
-            // here is our splitter ! We use ";" as a delimiter for each request
-            // then we are sure to have well formed statements
-            String[] inst = sb.toString().split(";");
-
-            Connection c = Database.getConnection();
-            Statement st = c.createStatement();
-
-            for (String anInst : inst) {
-                // we ensure that there is no spaces before or after the request string
-                // in order to not execute empty statements
-                if (!anInst.trim().equals("")) {
-                    st.executeUpdate(anInst);
-                    System.out.println(">>" + anInst);
-                }
-            }
-
-        } catch (Exception e) {
-            System.out.println("*** Error : " + e.toString());
-            System.out.println("*** ");
-            System.out.println("*** Error : ");
-            e.printStackTrace();
-            System.out.println("################################################");
-            System.out.println(sb.toString());
+        BufferedReader br = new BufferedReader(fr);
+        String line;
+        while ((line = br.readLine()) != null) {
+            sb.append(line);
         }
+        br.close();
 
+        // here is our splitter ! We use ";" as a delimiter for each request
+        // then we are sure to have well formed statements
+        String[] inst = sb.toString().split(";");
+
+        Connection c = Database.getConnection();
+        Statement st = c.createStatement();
+
+        for (String anInst : inst) {
+            // we ensure that there is no spaces before or after the request string
+            // in order to not execute empty statements
+            if (!anInst.trim().equals("")) {
+                st.executeUpdate(anInst);
+                System.out.println(">>" + anInst);
+            }
+        }
     }
 }
--- updatetracker-lib/src/test/java/dk/statsbiblioteket/doms/updatetracker/improved/worklog/WorkLogPollIT.java
+++ updatetracker-lib/src/test/java/dk/statsbiblioteket/doms/updatetracker/improved/worklog/WorkLogPollIT.java
@@ -22,32 +22,42 @@ import static org.mockito.Mockito.verifyNoMoreInteractions;
 public class WorkLogPollIT {
 
     @Test
-    public void testGetFedoraEvents() throws Exception {
+    public void testGetEventFromDockerWorklog() throws Exception {
         Properties props = new Properties();
         props.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("test.properties"));
-
-
-
-        Database.init(props.getProperty("fedora.worklog.database.URL"),
-                props.getProperty("fedora.worklog.database.username"),
-                props.getProperty("fedora.worklog.database.password"));
-        Database.executeSQL(new File(Thread.currentThread().getContextClassLoader().getResource("updatetrackerLogs.ddl").toURI()));
         UpdateTrackingConfig config = new UpdateTrackingConfig(props);
 
+        //Create the worklog table in the docker postgres database
+        Database.init(config.getFedoraDatabaseURL(),
+                config.getFedoraDatabaseUsername(),
+                config.getFedoraDatabasePassword());
+        Database.executeSQL(new File(Thread.currentThread().getContextClassLoader().getResource("updatetrackerLogs.ddl").toURI()));
+
+        //Mock the thing that the workLogpoller calls
+        UpdateTrackerPersistentStore persistentStore = mock(UpdateTrackerPersistentStoreImpl.class);
+
+        //Create the worklog dao
         WorkLogPollDAO workLogPollDAO = new WorkLogPollDAO(
                 config.getFedoraDatabaseDriver(),
                 config.getFedoraDatabaseURL(),
                 config.getFedoraDatabaseUsername(),
                 config.getFedoraDatabasePassword());
-        UpdateTrackerPersistentStore persistentStore = mock(UpdateTrackerPersistentStoreImpl.class);
+
+        //Create the poll task, notice the delay of 1 second. Entries younger than that are ignorede
         WorkLogPollTask pollTask = new WorkLogPollTask(workLogPollDAO, persistentStore, 10, 1000);
         pollTask.run();
+        //We expect no work units to be found, as the recently created entry should be to young
         verify(persistentStore).getLatestKey();
         verifyNoMoreInteractions(persistentStore);
+
+        //Reset the mock so the verifys start from 0 again
         reset(persistentStore);
+        //Sleep for long enough that the one event in the worklog is old enough
         Thread.sleep(1000);
+        //Start the task again
         pollTask.run();
         verify(persistentStore).getLatestKey();
+        //This time we expect the event to be found
         verify(persistentStore).datastreamChanged(eq("doms:testpid"), any(Date.class), eq("EVENTS"), eq(1L));
         verifyNoMoreInteractions(persistentStore);
 
